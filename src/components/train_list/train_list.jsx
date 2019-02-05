176616d931bc7144e5617f8b5df34ffb83bb9060
JoniT <jonit44@gmail.com>
2019-02-05 15:05:35 +0200

    part of train lists UI

--- src/components/train_list/train_list.jsx
+++ src/components/train_list/train_list.jsx
@@ -3,7 +3,27 @@ import Style from "./style.scss";
 
 const TrainList = props => (
   <div className="train_list">
-    <ul>{props.trains}</ul>
+    <div className="train_list_pagination">
+      <div className="train_list_pagination_entry">Saapuvat</div>
+      <div className="train_list_pagination_entry">Lähtevät</div>
+    </div>
+    <table className="train_list_trains">
+      <colgroup>
+        <col className="train_list_col_train" />
+        <col className="train_list_col_departure" />
+        <col className="train_list_col_arrival" />
+        <col className="train_list_col_arrival_time" />
+      </colgroup>
+      <tbody>
+        <tr className="train_list_row">
+          <th className="train_list_cell">Juna</th>
+          <th className="train_list_cell">Lahtöasema</th>
+          <th className="train_list_cell">Pääteasema</th>
+          <th className="train_list_cell">Saapuu</th>
+        </tr>
+        {props.trains}
+      </tbody>
+    </table>
   </div>
 );
 
